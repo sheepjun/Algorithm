e9daed1801a428992da947bbacc3cf33ad58aa73
양희준 <>
2021-04-30 03:29:30 +0900

    2019kakao 오픈채팅문제

--- src/kakao2019/ait.java
+++ src/kakao2019/ait.java
@@ -1,2 +1,39 @@
-package kakao2019;public class ait {
+package kakao2019;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ait {
+    static class a {
+        int a;
+        int b;
+
+        public a(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        List<a> list = new ArrayList<>();
+        int total = 5;
+        int[] stage = {5,4,2,3,3};
+        int[] user = new int[5];
+        for (int i = 0; i<5; i++) {
+            user[i] = stage[i];
+        }
+        for (int i = 0; i < 5; i++) {
+            if (user[i] == 0) {
+                list.add(new a(1, 0));
+            } else {
+                list.add(new a(i + 1, stage[i]));
+            }
+        }
+        for (a s : list) {
+            System.out.println(s.a);
+        }
+        System.out.println(list.get(1).a);
+    }
 }
--- src/kakao2019/test1.java
+++ src/kakao2019/test1.java
@@ -1,2 +1,53 @@
-package kakao2019;public class test1 {
+package kakao2019;
+
+
+import java.util.*;
+
+public class test1 {
+
+    //오픈채팅방
+    static String[] recorde = {
+            "Enter uid1234 Muzi", "Enter uid4321 Prodo",
+            "Leave uid1234 Muzi", "Enter uid1234 Prodo",
+            "Change uid4321 Ryan"
+    };
+    public static String[] solution(String[] recorde) {
+
+        List<String> list = new ArrayList<>();
+
+        Map<String, String> idmap = new HashMap<>();
+
+        for (String str : recorde) {
+            StringTokenizer tokenizer = new StringTokenizer(str);
+            String cmd = tokenizer.nextToken();
+            if (cmd.equals("Enter") || cmd.equals("Change")) {
+                String id = tokenizer.nextToken();
+                String name = tokenizer.nextToken();
+                idmap.put(id, name);
+            }
+        }
+
+        for (String str : recorde) {
+            StringTokenizer tokenizer = new StringTokenizer(str);
+            String cmd = tokenizer.nextToken();
+            if (cmd.equals("Enter")) {
+                String id = tokenizer.nextToken();
+                list.add(idmap.get(id) + "님이 들어왔습니다.");
+            } else if (cmd.equals("Leave")) {
+                String id = tokenizer.nextToken();
+                list.add(idmap.get(id) + "님이 나갔습니다.");
+            }
+        }
+
+        String[] ans = new String[list.size()];
+        list.toArray(ans);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String[] a = solution(recorde);
+        System.out.println(Arrays.toString(a));
+    }
+
+
 }
